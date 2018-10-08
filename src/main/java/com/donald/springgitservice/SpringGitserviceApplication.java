0517851931077dba59e1c6a525467214e4049d25
donald <donald.choi@kakaocorp.com>
2018-10-08 10:27:15 +0900

    프로젝트 생성

---
+++ src/main/java/com/donald/springgitservice/SpringGitserviceApplication.java
@@ -0,0 +1,12 @@
+package com.donald.springgitservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitserviceApplication.class, args);
+	}
+}
