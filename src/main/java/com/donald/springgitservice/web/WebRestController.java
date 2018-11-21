fca874ac52dc55ccc636461779d20fa2972cfcec
donald <donald@kakaocorp.com>
2018-11-21 13:34:00 +0900

    develop 커밋

--- src/main/java/com/donald/springgitservice/web/WebRestController.java
+++ src/main/java/com/donald/springgitservice/web/WebRestController.java
@@ -1,4 +1,4 @@
-package com.donald.springgitservice.web;
+/*package com.donald.springgitservice.web;
 
 import com.donald.springgitservice.domain.posts.Posts;
 import com.donald.springgitservice.domain.posts.PostsRepository;
@@ -7,10 +7,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @AllArgsConstructor
@@ -24,6 +23,43 @@ public class WebRestController {
 	public String hello() {
 		System.out.println("Git Test.222");
 		return "helloWorld";
+	}
+
+	@PostMapping("/posts")
+	public void savePosts(@RequestBody PostsSaveRequestDto dto){
+		postsRepository.save(dto.toEntity());
+	}
+
+}*/
+
+package com.donald.springgitservice.web;
+
+		import com.donald.springgitservice.domain.posts.Posts;
+		import com.donald.springgitservice.domain.posts.PostsRepository;
+		import com.donald.springgitservice.domain.posts.PostsSaveRequestDto;
+		import lombok.AllArgsConstructor;
+		import lombok.Getter;
+		import lombok.NoArgsConstructor;
+		import lombok.Setter;
+		import org.springframework.web.bind.annotation.*;
+
+		import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("user")
+public class WebRestController {
+
+	// 스프링 Bean 주입 방식 -> @Autowires, setter, 생성자
+	// 생성자 주입 방식 권장 (= @AllArgsConstructor)
+	private PostsRepository postsRepository;
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
+	public HttpServletResponse hello(HttpServletResponse response) throws Exception{
+		response.setStatus(500, "500 Error");
+
+		throw new Exception();
+
 	}
 
 	@PostMapping("/posts")
