f1dc3ea50600dedfda6a37108f6e5ff4f991e0ac
donald-choi <donald.choi@kakaocorp.com>
2019-01-28 14:50:42 +0900

    commit

--- src/main/java/com/donald/springgitservice/web/WebRestController.java
+++ src/main/java/com/donald/springgitservice/web/WebRestController.java
@@ -22,8 +22,7 @@ public class WebRestController {
 	@GetMapping("/hello")
 	public String hello() {
 
-		System.out.println("Git Kraken First donald to donald2");
-		System.out.println("Git Kraken First donald to donald2 : Edit file");
+		System.out.println("Git Kraken First donald2");
 
 		return "helloWorld";
 	}
